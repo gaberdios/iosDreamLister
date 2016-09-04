bceb2153eacbed75563d2b0ca6bf9dd312999908
gaberdios <gaberdios@gmail.com>
2016-09-04 21:24:04 +0200

    Part 3

---
+++ DreamLister/ItemCell.swift
@@ -0,0 +1,19 @@
+//
+//  ItemCell.swift
+//  DreamLister
+//
+//  Created by G on 04/09/16.
+//  Copyright © 2016 gaberdios. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet weak var thumb: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
+
+}
